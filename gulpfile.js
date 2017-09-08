3a5a7d2fd09872be415c47fdd2351e1b3be1447c
ValeriyKobysh <vmksocnet@outlook.com>
2017-09-08 17:20:57 +0300

    gulp: gulp-sass
    Install and config gulp-sass, gulp-rename and gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 const gulp = require('gulp'),
       pug = require('gulp-pug');
 
+const sass = require('gulp-sass'),
+      rename = require('gulp-rename'),
+      sourcemaps = require('gulp-sourcemaps');
+
 const paths = {
     root: './dist',
     templates: {
         pages: 'src/views/*.pug',
-        // src: 'src/views/'
         dist: 'dist/'
+    },
+    styles: {
+        source: './src/sass/index.sass',
+        dist: 'dist/css'
     }
 }
 
 /**
- * ***Pug***
+ * ***PUG***
  */
 function templates() {
     return gulp.src(paths.templates.pages)
@@ -19,4 +26,17 @@ function templates() {
         .pipe(gulp.dest(paths.root));
 }
 
+/**
+ * ***SASS***
+ */
+function styles() {
+    return gulp.src(paths.styles.source)
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sourcemaps.write())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(paths.styles.dist))
+}
+
 exports.templates = templates;
+exports.styles = styles;
